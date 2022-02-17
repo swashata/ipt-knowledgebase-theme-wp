d1ed2b1ca533970be5fbb669ba0d98bb91f283ae
Chris Gilligan <49878588+UTCGilligan@users.noreply.github.com>
2022-02-17 12:34:55 -0500

    Update class-ipt-kb-knowledgebase-widget.php

--- inc/class-ipt-kb-knowledgebase-widget.php
+++ inc/class-ipt-kb-knowledgebase-widget.php
@@ -144,4 +144,4 @@ class IPT_KB_KnowledgeBase_Widget extends WP_Widget {
 	}
 }
 
-add_action( 'widgets_init', create_function( '', "register_widget( 'IPT_KB_KnowledgeBase_Widget' );" ) );
+add_action( 'widgets_init', function() { register_widget( "IPT_KB_KnowledgeBase_Widget" ); } );
